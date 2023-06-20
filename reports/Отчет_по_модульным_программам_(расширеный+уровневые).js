5754fb3ef095a9618ad58272f8f0d4d89e34146e
Rodin Stasnislav <65771468+rodin-stas@users.noreply.github.com>
2023-06-20 16:34:19 +0300

    add report

---
+++ reports/Отчет_по_модульным_программам_(расширеный+уровневые).js
@@ -0,0 +1,174 @@
+function addColumn(name, value){
+	// Создается настраиваемого отчета .
+	column = columns.AddChild();
+	// Заголовок столбца 
+	column.flag_formula = true;
+	column.column_title = name;
+	// Значения данных - данные
+	column.column_value = value;
+	column.column_width = 200;
+	column.column_width_unit = "px";
+	// Тип данных столбца настраиваемого отчета - строка
+	column.datatype = "string";
+}
+
+function addTaskColumn(count, type){
+	if (type == 'education_method') {
+		addColumn(count+"-й этап мод. программы","ListElem.name"+count)
+		addColumn("Тип "+count+"-го этапа мод. программы","ListElem.type"+count)
+		addColumn("Статус","ListElem.state"+count)
+		addColumn("Присутствовал","ListElem.event_is_assist"+count)
+		addColumn("Дата начала мероприятия","ListElem.event_start_date"+count)
+	} else {
+		addColumn(count+"-й этап мод. программы","ListElem.name"+count)
+		addColumn("Тип "+count+"-го этапа мод. программы","ListElem.type"+count)
+		addColumn("Статус","ListElem.state"+count)
+	}
+
+}
+
+function processModProgramCriterions(){
+	comProgStr = "";
+	isFirst = true;
+	for (i=0;i<5;i++){
+		comProgCriterion = criterions[i];
+		if (comProgCriterion!=undefined){
+			value = OptInt(comProgCriterion.value,0);
+			if (value != 0){
+				if (isFirst){
+					isFirst = false;
+				}else{
+					comProgStr+= ",";
+				}
+				comProgStr+=value;
+			}
+		}
+	}
+	if (isFirst)
+		return "and 1=0";
+	else return "and cps.id in ("+comProgStr+")";
+}
+
+function processDateCriterions(){
+	fromDate = OptDate(criterions[5].value);
+	toDate = criterions[6].value;
+	if (fromDate != undefined && toDate != undefined)
+		return "where eps.create_date between convert(datetime, '"+fromDate+"',104) and convert(datetime, '"+toDate+"',104) "
+	return ""
+}
+
+function processCollaboratorCriterions(){
+		colID = OptInt(criterions[7].value,0);
+		if (colID !=0)
+			return " and cs.id = "+colID+" ";
+		return "";
+}
+
+aRes = [];
+progFilter = processModProgramCriterions();
+dateFilter = processDateCriterions();
+colFilter = processCollaboratorCriterions();
+
+try {
+	arr = XQuery("sql:  \n\
+	select DISTINCT \n\
+		eps.id 'eps_id', \n\
+		eps.person_fullname, \n\
+    	eps.person_id, \n\
+		cs.position_name, \n\
+		cs.login, \n\
+		eps.person_id, \n\
+		cps.name, \n\
+		cps.code, \n\
+		cps.id 'cps_id', \n\
+		eps.create_date, \n\
+		cpext.with_cancel, \n\
+		DATEADD(DAY,cps.duration,eps.create_date) 'end_date', \n\
+		datediff(day,GETDATE(),DATEADD(DAY,cps.duration,eps.create_date)) 'remaining', \n\
+		g.name 'group_name' \n\
+	from \n\
+		education_plans eps \n\
+		inner join collaborators cs on cs.id = eps.person_id "+colFilter+" \n\
+		inner join compound_programs cps on cps.id = eps.compound_program_id " +progFilter+ " \n\
+		inner join compound_programs_ext cpext on cps.id = cpext.id \n\
+		left join groups g on g.id = eps.group_id "+dateFilter 
+	);
+	
+	addColumn("Название должности","ListElem.position_name")
+	addColumn("Табельный номер","ListElem.tab_number")
+	addColumn("Наименование мод.программы","ListElem.name")
+	addColumn("Код мод.программы","ListElem.code")
+	addColumn("Дата назначения Программы сотруднику","ListElem.create_date")
+	addColumn("Плановый срок прохождения мод. программы","ListElem.end_date")
+	addColumn("Осталось дней на прохождение","ListElem.remaining")
+	addColumn("Формат уведомления (с отменой / без)","ListElem.with_cancel")
+	addColumn("Статус Плана обучения","ListElem.plan_state")
+	addColumn("Наименование группы","ListElem.group_name")
+	addColumn("Количество неявок на мероприятия","ListElem.count")
+	addColumn("Даты неявок на мероприятия","ListElem.dates")
+	
+	columns_count =0;
+	for (plan in arr){
+	
+		tasks_count = 0;
+		r = new Object();
+		r.SetProperty("PrimaryKey",plan.eps_id);
+		r.SetProperty("person_fullname",plan.person_fullname);
+		r.SetProperty("position_name",plan.position_name);
+		r.SetProperty("tab_number",plan.login);
+		r.SetProperty("name",plan.name);
+		r.SetProperty("code",plan.code);
+		r.SetProperty("create_date",plan.create_date);
+		r.SetProperty("end_date",plan.end_date);
+		r.SetProperty("remaining",plan.remaining);
+		r.SetProperty("with_cancel", (plan.with_cancel == true ? "Да" : "Нет"));
+	
+		modProgramDoc = OpenDoc(UrlFromDocID(OptInt(plan.eps_id))).TopElem;
+		r.SetProperty("plan_state",common.education_learning_states.GetChildByKey( OptInt( modProgramDoc.state_id ) ).name);
+		r.SetProperty("group_name",plan.group_name);
+		r.SetProperty("count",modProgramDoc.custom_elems.ObtainChildByKey('skipped_count').value);
+		r.SetProperty("dates",modProgramDoc.custom_elems.ObtainChildByKey('skipped_dates').value);
+		for (task in modProgramDoc.programs){
+			tasks_count++;
+			
+			r.SetProperty("name"+tasks_count,task.name);
+
+			if(task.type == 'folder') {
+				r.SetProperty("type"+tasks_count, "Этап")
+			} else {
+				r.SetProperty("type"+tasks_count,common.exchange_object_types.GetOptChildByKey( task.type ).title);
+			}
+
+			
+            if(task.type == 'education_method') {
+				eventResult = ArrayOptFirstElem(XQuery("sql: \n\
+					select  \n\
+						[t2].[event_start_date], \n\
+						[t2].[is_assist] \n\
+					FROM \n\
+						[education_methods] AS [t0] \n\
+						INNER JOIN [events] AS [t1] ON [t1].[education_method_id] = [t0].[id] \n\
+						INNER JOIN [event_results] AS [t2] ON [t2].[event_id] = [t1].[id] \n\
+					WHERE	 \n\
+						[t0].[id] = "+SqlLiteral(task.object_id)+" \n\
+						AND [t2].[person_id] = "+SqlLiteral(plan.person_id)+" \n\
+						AND [t2].[is_assist] = 1  \n\
+					"))
+
+                r.SetProperty("event_is_assist"+tasks_count, eventResult != undefined ? '+' : '');
+				r.SetProperty("event_start_date"+tasks_count, eventResult != undefined ? eventResult.event_start_date : '');
+            }
+
+			r.SetProperty("state"+tasks_count,common.learning_states.GetOptChildByKey( Int( task.state_id ) ).name);
+			
+			if (columns_count < tasks_count){
+				columns_count++;
+				addTaskColumn(columns_count, task.type );
+			}
+		}
+		aRes.push(r);
+	}
+	return aRes;
+} catch(err) {
+	alert("err = " + err)
+}
